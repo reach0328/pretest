74ab7d02e0011306bb9068cb53e424c49480f6cf
JH\JH <reach0328@gmail.com>
2017-04-17 15:21:08 +0900

    test1

--- src/pretest/test1.java
+++ src/pretest/test1.java
@@ -1,5 +1,27 @@
 package pretest;
 
-public class test1 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class test1 {
+	public static void main(String[] args) {
+		int read[] = {13,14,15,16,20,23,24,25,100};
+		int temp = read[0];
+		String show = ""+temp;
+		int count = 1;
+		for(int i = 1; i < read.length;i++) {
+			temp = temp+1;
+			count++;
+			if(temp != read[i] && i !=1){
+				if(count>2){
+					count=1;
+					show = show +"~"+ read[i-1];
+				}
+				temp = read[i];
+				show = show +", "+ read[i];
+			}
+		}
+		System.out.println(show);
+	}
 }
---
+++ src/pretest/test2.java
@@ -0,0 +1,14 @@
+package pretest;
+
+public class test2 {
+	public static void main(String[] args) {
+		String temp = "The quick <font color=\"brown\">brown</font> fox jumps over the <i>lazy</i> dog";
+		
+		
+		String result = "";
+		
+		
+		System.out.println(result);
+	}
+
+}
